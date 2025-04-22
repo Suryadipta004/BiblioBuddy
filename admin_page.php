71a3c76d81564481fc79f97433228a5a0083169f
Suryadipta004 <suryadiptadas.2020@gmail.com>
2025-04-22 17:22:44 +0530

    admin

---
+++ admin_page.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bibliobuddy</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="icon" href="images/favicon.png" type="image/png">
+    <link rel="stylesheet" href="css/admin_style.css">
+
+</head>
+
+<body>
+    <?php include 'admin_header.php' ?>
+
+</body>
+
+</html>
